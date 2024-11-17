95806c8bc862636b55ecaf8b9c25b12f50cd2bad
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2024-11-17 21:35:33 +0200

    Some refactor

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,13 +8,11 @@ const Body = () => {
       {/* Paste your card components here like this <Card name="..." position="..." /> */}
       <Card name="Vlad Kuryliv" position="QA Engineer" />
       <Card name="Kurman Vitalii" position="Software Engineer" />
-      <Card name="Tania Medvid" position="Business analyst"/>
+      <Card name="Tania Medvid" position="Business analyst" />
       <Card name="Sofiia Kuhivchak" position="Project manager" />
       <Card name="Vitaliy Kormylo" position="Frontend Developer" />
-
     </main>
   );
 };
 
 export default Body;
-  
